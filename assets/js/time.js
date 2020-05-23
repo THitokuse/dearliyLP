4914683a9efcc610e6f22f71046043cd520fc4f3
THitokuse <hasegawa.taichiro@gmail.com>
2020-05-23 13:32:27 +0900

    add time.js

---
+++ assets/js/time.js
@@ -0,0 +1,16 @@
+$(function() {
+  const now = new Date();
+  const hour = now.getHours();
+
+  if (6 < hour < 18) {
+    $('header').css('backgroundColor', '#f6efe7');
+    $('footer').css('backgroundColor', '#f6efe7');
+    $('.title-text').css('color', '#F0831E');
+    $('.logo').children('img').attr('src', './assets/images/Orange-01.png');
+  } else {
+    $('header').css('backgroundColor', '#d3def0');
+    $('footer').css('backgroundColor', '#d3def0');
+    $('.title-text').css('color', 'blue');
+    $('.logo').children('img').attr('src', './assets/images/Blue-02.png');
+  }
+})
