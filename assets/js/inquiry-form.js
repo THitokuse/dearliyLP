fff6799a94d474af4ccf9cc6012fc571abff2959
THitokuse <hasegawa.taichiro@gmail.com>
2020-05-24 18:00:58 +0900

    add inquiry-form.js

--- assets/js/inquiry-form.js
+++ assets/js/inquiry-form.js
@@ -6,6 +6,39 @@ $(function() {
   });
   $('#submit').on('click', function(e){
     e.preventDefault();
+
+    let queryString = window.location.search;
+    var queryObject = new Object();
+    if(queryString){
+      queryString = queryString.substring(1);
+      const parameters = queryString.split('&');
+    
+      for (let i = 0; i < parameters.length; i++) {
+        let element = parameters[i].split('=');
+    
+        let paramName = decodeURIComponent(element[0]);
+        let paramValue = decodeURIComponent(element[1]);
+  
+        queryObject[paramName] = paramValue;
+      }
+    }
+    const message = `
+    【Dearliyお問い合わせ】
+      名前： ${queryObject.family_name + queryObject.first_name}
+      メールアドレス： ${queryObject.email}
+      内容： ${queryObject.content}
+    `;
+
+    // slackへの通知
+    const url = 'https://hooks.slack.com/services/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';
+    const data = {
+      text: message
+    };
+    const xml = new XMLHttpRequest();
+    xml.open("POST", url, false);
+    xml.setRequestHeader("content-type", "application/x-www-form-urlencoded;charset=UTF-8");
+    xml.send(`payload=${JSON.stringify(data)}`)
+
     location.href='/inquiry/thanks/index.html';
   })
   $('#cancel').on('click', function(e){
