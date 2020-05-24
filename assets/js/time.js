875292b7c681ed33a2978cb434992e8ca0b7f393
THitokuse <hasegawa.taichiro@gmail.com>
2020-05-24 16:51:35 +0900

    modify 絶対パス

--- assets/js/time.js
+++ assets/js/time.js
@@ -6,11 +6,11 @@ $(function() {
     $('header').css('backgroundColor', '#f6efe7');
     $('footer').css('backgroundColor', '#f6efe7');
     $('.title-text').css('color', '#F0831E');
-    $('.logo').children('img').attr('src', './assets/images/Orange-01.png');
+    $('.logo').children('img').attr('src', 'https://dearliy-lp.herokuapp.com/assets/images/Orange-01.png');
   } else {
     $('header').css('backgroundColor', '#d3def0');
     $('footer').css('backgroundColor', '#d3def0');
     $('.title-text').css('color', 'blue');
-    $('.logo').children('img').attr('src', './assets/images/Blue-02.png');
+    $('.logo').children('img').attr('src', 'https://dearliy-lp.herokuapp.com/assets/images/Blue-02.png');
   }
 })
