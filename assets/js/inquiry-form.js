6ceb43e9fe01cb7b4562eb984debebab6e0e997b
THitokuse <hasegawa.taichiro@gmail.com>
2020-05-24 16:51:57 +0900

    create inquiry-form.js

---
+++ assets/js/inquiry-form.js
@@ -0,0 +1,32 @@
+$(function() {
+  $('#confirm').on('click',function(e){
+    e.preventDefault();
+    const formData = $('form').serialize();
+    location.href=`/inquiry/index.html?${formData}`;
+  });
+  $('#submit').on('click', function(e){
+    e.preventDefault();
+    location.href='/inquiry/thanks/index.html';
+  })
+  $('#cancel').on('click', function(e){
+    e.preventDefault();
+    const formData = $('form').serialize();
+    location.href=`/?${formData}`;
+  })
+
+  // お問い合わせ確認ページ値入力
+  let queryString = window.location.search;
+  if(queryString){
+    queryString = queryString.substring(1);
+    const parameters = queryString.split('&');
+  
+    for (let i = 0; i < parameters.length; i++) {
+      let element = parameters[i].split('=');
+  
+      let paramName = decodeURIComponent(element[0]);
+      let paramValue = decodeURIComponent(element[1]);
+
+      document.getElementById(paramName).value = paramValue;
+    }
+  }
+})
