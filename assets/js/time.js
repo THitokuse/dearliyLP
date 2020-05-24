b05bfcf71533efa2b108f1b1a265264eba149d4b
THitokuse <hasegawa.taichiro@gmail.com>
2020-05-24 18:08:54 +0900

    modify night-mode

--- assets/js/time.js
+++ assets/js/time.js
@@ -2,14 +2,16 @@ $(function() {
   const now = new Date();
   const hour = now.getHours();
 
-  if (6 < hour < 18) {
+  if (6 < hour && hour < 18) {
     $('header').css('backgroundColor', '#f6efe7');
     $('footer').css('backgroundColor', '#f6efe7');
+    $('.btn').css('backgroundColor', '#F0831E');
     $('.title-text').css('color', '#F0831E');
     $('.logo').children('img').attr('src', 'https://dearliy-lp.herokuapp.com/assets/images/Orange-01.png');
   } else {
     $('header').css('backgroundColor', '#d3def0');
     $('footer').css('backgroundColor', '#d3def0');
+    $('.btn').css('backgroundColor', 'blue');
     $('.title-text').css('color', 'blue');
     $('.logo').children('img').attr('src', 'https://dearliy-lp.herokuapp.com/assets/images/Blue-02.png');
   }
